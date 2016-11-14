502e98f0e2d864a61550dcdb0fed7ee5571d3652
Jefferson Wu <makerjeffwu@gmail.com>
2016-11-13 21:59:05 -0800

    latest and greatest

--- ffmpeg.js
+++ ffmpeg.js
@@ -17,6 +17,8 @@ var cmd = 'ffmpeg -i ' + inputFile + ' ' + outputFile;
 
 var fileArray = fs.readdirSync(process.cwd() + '/video_source/to_process');
 
+var inputFileName = process.argv[2];
+
 
 // run FFMPEG
 // child_process.exec(cmd, function(error, stdout, stderr){
@@ -29,18 +31,67 @@ var fileArray = fs.readdirSync(process.cwd() + '/video_source/to_process');
 // =======
 // RUNTIME
 // =======
-processArrayOfFiles(fileArray);
+//processArrayOfFiles(fileArray);
+processOneFile('IMG_0005.m4v');
 
 
 // ========
 // FUNCTION
 // ========
+
+function processOneFile(fileName) {
+
+    var outputFileName = fileName.split('.');
+    outputFileName.splice(1,1);
+
+    //run FFMPEG
+    child_process.exec('ffmpeg -i ' + __dirname + '/video_source/to_process/' + fileName + ' ' + __dirname + '/video_output/' + outputFileName + '.mp4',
+
+        function(err, stdout, stderr){
+
+            if(err) {
+                console.log('Error: ' + error);
+            }
+            else if (stderr) {
+                console.log('STDOUT: ' + stderr);
+            } else {
+                console.log(stdout);
+            }
+    });
+}
+/**
+ * Process files to MP4
+ * @param array Input Array.
+ */
 function processArrayOfFiles(array) {
-    clear();
-    console.log('## File List: ##');
+
     array.forEach(function(elem, ind, arr){
-        console.log(chalk.green(elem));
+
+        var outputFileName = elem.split('.');
+        outputFileName.splice(1,1);
+
+        console.log("Processing " + elem + '... ');
+        child_process.exec('ffmpeg -i ' + __dirname + '/video_source/to_process/' + elem + ' ' + outputFileName + '.mp4', function(error, stdout, stderr){
+            if(error) console.log('Error: ' + error);
+
+            console.log(stdout);
+        });
     });
 
+    process.exit();
 
+
+    //debugInfo(array);
+}
+
+/**
+ * Debug info
+ * @param array Input array.
+ */
+function debugInfo(array) {
+    clear();
+    console.log(chalk.yellow('## File List: ##'));
+    array.forEach(function(elem, ind, arr){
+        console.log('- ' + chalk.green(elem));
+    });
 }
