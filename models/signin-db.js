7e3ff13bc311ae4d29ebd6503d065e0ceda6ddcc
Jeff Wu <makerjeffwu@gmail.com>
2016-11-30 00:03:29 -0800

    latest and greatest on dev branch

--- models/signin-db.js
+++ models/signin-db.js
@@ -4,9 +4,11 @@
 
 const fs        = require('fs');
 const chalk     = require('chalk');
+const jwt       = require('jsonwebtoken');
 
 var passwordFile = fs.readFileSync(process.cwd() + '/password.txt', {encoding:'utf8'});
 var signingKey = fs.readFileSync(process.cwd() + '/signingkey.txt', {encoding: 'utf8'});
+var dataObject = {iss:'http://localhost', datum:'this is some data.'};
 
 
 module.exports.credentials = {
@@ -15,6 +17,12 @@ module.exports.credentials = {
     'signingkey': signingKey
 };
 
-function checkPasswordFile(){
+module.exports.createToken = function(exp){
 
-}
+    var secret = fs.readFileSync(process.cwd() + '/signingkey.txt');
+    var dataObject = {iss:'http://localhost', datum:'this is some data.'};
+    var token = jwt.sign(dataObject, secret, {expiresIn: exp});
+    console.log(token);
+    return token;
+
+};
