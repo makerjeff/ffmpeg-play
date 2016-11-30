a6f12c5146b6e0b87f55a9851e29b7f7b9168296
Jefferson Wu <makerjeffwu@gmail.com>
2016-11-29 16:09:11 -0800

    updated the signingkey.txt requirement in signin-db.js

--- models/signin-db.js
+++ models/signin-db.js
@@ -6,7 +6,7 @@ const fs        = require('fs');
 const chalk     = require('chalk');
 
 var passwordFile = fs.readFileSync(process.cwd() + '/password.txt', {encoding:'utf8'});
-var signingKey = fs.readFileSync(process.cwd() + '/signingKey.txt', {encoding: 'utf8'});
+var signingKey = fs.readFileSync(process.cwd() + '/signingkey.txt', {encoding: 'utf8'});
 
 
 module.exports.credentials = {
