9da2bd9cbd7aec8166e6ee1369da5eac302a7cd8
Jefferson Wu <makerjeffwu@gmail.com>
2016-12-09 10:54:13 -0800

    latest and greatest, about to create a Word Stats logger

---
+++ models/words-log-db.js
@@ -0,0 +1,28 @@
+/**
+ * Created by jefferson.wu on 12/9/16.
+ */
+
+//words.js (mongoose)
+//
+//     //-- grab mongoose --
+// var mongoose = require('mongoose');
+//
+// //-- define schema --
+// var userSchema = mongoose.Schema({
+//     email: String,
+//     password: String
+// });
+//
+// // -- schema methods -- IF ANY
+//
+//
+// // -- create model --
+// var User = mongoose.model('User', userSchema);
+//
+// // -- export module --
+// module.exports = User;
+
+
+
+
+//TODO: pseudo-code
