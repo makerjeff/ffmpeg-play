d170f06ef3aef7bc7b0e1dd50e408d20738ec76d
Jeff Wu <makerjeffwu@gmail.com>
2016-12-14 10:21:30 -0800

    latest and greatest, working factory

--- repeatwords-play.js
+++ repeatwords-play.js
@@ -15,7 +15,30 @@ files.forEach(function(elem, ind, arr){
 
     //strip extension
     var noExtFileName = elem.split('.').shift();
-    console.log(noExtFileName);
+    //console.log(noExtFileName);
 
+    //look for the '_' character
+    if (noExtFileName.search(/[_]/) === -1) {
+
+        var returnObject = {word: noExtFileName, index: 'master', filename: elem};
+
+        //console.log(returnObject);
+        cleanArray.push(returnObject);
+    } else {
+        returnObject = { word: noExtFileName, index: noExtFileName[noExtFileName.search(/[_]/) + 1] , filename: elem};
+        //console.log(returnObject);
+        cleanArray.push(returnObject);
+    }
 
 });
+
+// --- output ---
+cleanArray.forEach(function(elem, ind, arr){
+    if(elem.index === 'master') {
+        console.dir(elem);
+    } else {
+        //console.dir(elem);
+    }
+});
+
+//TODO: add duplicates to master word
