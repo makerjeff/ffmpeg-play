d59b45e25ecf1cee3c1062880ccc486e57a9d1ab
Jefferson Wu <makerjeffwu@gmail.com>
2016-12-14 12:54:06 -0800

    updated front-end sanitizer

--- public/js/textSanitizer.js
+++ public/js/textSanitizer.js
@@ -6,12 +6,33 @@ var textSanitizer = {
 
     /**
      * Get Sanitized String from a dirty string input.
-     * @param inputStr  Durrrty input string.
+     * @param dirtyString  Durrrty input string.
      * @returns {string|XML}    Cleaned output string.
      */
-    getSanitizedString: function(inputStr){
-        // basic chars, multi-spaces, brackets, trailing spaces, leading spaces
-        return inputStr.replace(/[0123456789!@#$%^&*()_+{}]/g, '').replace(/\[.*?\]/g,"").replace(/\s\s+/g, ' ').replace(/[ ]+$/g, '').replace(/^[ ]/g, '');
+    getSanitizedString: function(dirtyString){
+        //TODO: make less verbose for output.
+        // strip the numbers
+        var cleanString = dirtyString.replace(/[0-9]/g, '');
+
+        // strip special characters
+        cleanString = cleanString.replace(/[~`!@#$%^&*()_+-=<>?,./]/g, '');
+
+        // strip backslashes
+        cleanString = cleanString.replace(/\\/g, '');
+
+        // replace backslashes
+        cleanString = cleanString.replace(/\{\}/g, '');
+
+        // replace multi-spaces with single space
+        cleanString = cleanString.replace(/\s\s+/g, ' ');
+
+        // strip trailing spaces
+        cleanString = cleanString.replace(/\s$/g, '');
+
+        // strip leading spaces
+        cleanString = cleanString.replace(/^\s/g, '');
+
+        return cleanString;
     }
 };
 
